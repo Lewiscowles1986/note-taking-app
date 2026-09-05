0508c7985553457c1ddb64f70c5731ec85c65bdb
Lewiscowles1986 <Lewiscowles1986@users.noreply.github.com>
2026-09-05 20:14:32 +0000

    deploy: c9db52607d782a1be96157a71989cdfb72048e6d

---
+++ preview-builds/renovate-lucide-monorepo/assets/swimlanesDiagram-VR7AAH4N-ChFvnHge.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-D5NzDZGm.js";import{_ as a}from"./MermaidBlock-BTKPcQsi.js";import"./chunk-5VM5RSS4-CVQAPM1S.js";import"./chunk-XXDRQBXY-C6DncxWG.js";import"./chunk-POPQ4Y6H-BYXOJhBq.js";import"./chunk-F27PBJKO-CRvMXDxL.js";import"./channel-CM0zQ4Hl.js";import"./index-DN1Bzbzo.js";import"./workflow-2vd2D4Uy.js";import"./copy-41Ix_khx.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
