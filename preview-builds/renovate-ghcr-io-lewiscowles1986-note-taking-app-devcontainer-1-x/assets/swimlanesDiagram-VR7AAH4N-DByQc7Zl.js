fdb34bd4869f05029b344a81e90d7532e17f3f3b
Lewiscowles1986 <Lewiscowles1986@users.noreply.github.com>
2026-09-05 19:22:18 +0000

    deploy: 57cf14c9bad7f569847f0ebc3cdde1e1243c1aca

---
+++ preview-builds/renovate-ghcr-io-lewiscowles1986-note-taking-app-devcontainer-1-x/assets/swimlanesDiagram-VR7AAH4N-DByQc7Zl.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-Bm7h8tZJ.js";import{_ as a}from"./MermaidBlock-CIVcmcT4.js";import"./chunk-5VM5RSS4-BgS-OfyR.js";import"./chunk-XXDRQBXY-CZBEdB0T.js";import"./chunk-POPQ4Y6H-C0jsyj7R.js";import"./chunk-F27PBJKO-BP6j_YSj.js";import"./channel-HI6eth8u.js";import"./index-D_EiJPR4.js";import"./workflow-DOEQ5VrB.js";import"./copy-CEWslFkK.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
