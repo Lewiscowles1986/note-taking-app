41415ac5f1db235d8d0a88352d6fbe458e4cccc4
Lewiscowles1986 <Lewiscowles1986@users.noreply.github.com>
2026-09-07 16:23:41 +0000

    deploy: bdab19652ae90d1db472775fcb8239768aead9f5

---
+++ preview-builds/docs-improved-custom-editor-docs/assets/channel-VC3GHfo0.js
@@ -0,0 +1,1 @@
+import{ai as o,aj as n}from"./MermaidBlock-BVZjIb1c.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ preview-builds/docs-improved-custom-editor-docs/assets/swimlanesDiagram-VR7AAH4N-9o3Uk4dC.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-CZJuZdIC.js";import{_ as a}from"./MermaidBlock-BVZjIb1c.js";import"./chunk-5VM5RSS4-7mdJEl_R.js";import"./chunk-XXDRQBXY-BRLENv1q.js";import"./chunk-POPQ4Y6H-JtkyoX_1.js";import"./chunk-F27PBJKO-iNT8aeRX.js";import"./channel-VC3GHfo0.js";import"./index-BmvB2njE.js";import"./workflow-DT2o2Ink.js";import"./copy-DCBTnPuJ.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
