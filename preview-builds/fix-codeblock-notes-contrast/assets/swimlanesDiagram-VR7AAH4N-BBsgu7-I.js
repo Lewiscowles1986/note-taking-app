4f3c780e524ca75dd6c503ba13836c63b63d6e98
Lewiscowles1986 <Lewiscowles1986@users.noreply.github.com>
2026-09-09 18:09:19 +0000

    deploy: d0b5f8a3b6d1312a44e1fc129333fd3f2ce5fa53

---
+++ preview-builds/fix-codeblock-notes-contrast/assets/swimlanesDiagram-VR7AAH4N-BBsgu7-I.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-z8jZjNsU.js";import{_ as a}from"./MermaidBlock-1KWY_d6A.js";import"./chunk-5VM5RSS4-DNaBt27m.js";import"./chunk-XXDRQBXY-DW-MpZIQ.js";import"./chunk-POPQ4Y6H-B8_DQbId.js";import"./chunk-F27PBJKO-iOiGw9A0.js";import"./channel-Dx0uGE52.js";import"./index-DCq_xspz.js";import"./workflow-CsI4B4vK.js";import"./copy-DgyzRgW8.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,_=r({defaultLayout:"swimlane",styles:m});export{_ as diagram};
---
+++ preview-builds/fix-codeblock-notes-contrast/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/note-taking-app/preview-builds/fix-codeblock-notes-contrast/sw.js', { scope: '/note-taking-app/preview-builds/fix-codeblock-notes-contrast/' })})}
